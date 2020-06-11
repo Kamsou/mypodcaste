7c2277892ba0af1967fb264b8a99a180337c0b74
Camille <camille.coutens@gmail.com>
2020-06-11 22:29:07 +0200

    play react navigation

---
+++ src/navigators/MainStackNavigator.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from '../components/HomeScreen';
+
+const MainStack = createStackNavigator();
+
+const MainStackNavigator = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={HomeScreen} />
+    </MainStack.Navigator>
+  )
+}
+
+export default MainStackNavigator;
+
+
